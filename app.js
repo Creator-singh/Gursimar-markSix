9c0df1fd534646f121fcb4c64c9d8058e4c8b371
Creator-singh <64949453+Creator-singh@users.noreply.github.com>
2021-09-16 17:45:20 +0530

    reference to js and web page UI added

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.querySelector("#button-translate");
+var txtInput = document.querySelector("#text-input");
+
+
+btnTranslate.addEventListener("click", function clickHandler() {
+    console.log("clicked")
+    console.log(txtInput.value)
+})
