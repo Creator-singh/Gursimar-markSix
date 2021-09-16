bcd6e91a0773552c89257177f2c424cc888fe695
Creator-singh <64949453+Creator-singh@users.noreply.github.com>
2021-09-16 22:36:19 +0530

    feat: linked application with API

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var btnTranslate = document.querySelector("#button-translate");
 var txtInput = document.querySelector("#text-input");
-var txtoutput = document.querySelector("#text-output");
+var txtOutput = document.querySelector("#text-output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
-btnTranslate.addEventListener("click", function clickHandler() {
-    txtoutput.innerText = "nfbfb " + txtInput.value;
-})
+function generateURL(inputText){
+    return serverURL + "?" + "text=" + inputText;
+}
+
+function errorHandler(error){
+    alert("Something went wrong! Try again later.")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(generateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText; 
+        })
+        .catch(errorHandler)
+};
+
+
+btnTranslate.addEventListener("click", clickHandler);
