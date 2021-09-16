2a340e425980461bd8f0ffede71a1dc584a6192c
Creator-singh <64949453+Creator-singh@users.noreply.github.com>
2021-09-16 19:25:19 +0530

    simple input and output

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btnTranslate = document.querySelector("#button-translate");
 var txtInput = document.querySelector("#text-input");
+var txtoutput = document.querySelector("#text-output");
 
 
 btnTranslate.addEventListener("click", function clickHandler() {
-    console.log("clicked")
-    console.log(txtInput.value)
+    txtoutput.innerText = "nfbfb " + txtInput.value;
 })
